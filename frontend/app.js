4bb53d431c3f3b15aa2d80321fb59e4a175b1640
Johannes Luderschmidt <lagerkoller@vildan.de>
2017-11-14 18:55:19 +0100

    Fix link in first image.

--- frontend/app.js
+++ frontend/app.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     let posts = [
         {
             "id": "d92jfi",
-            "image": "http://localhost:3000/uploads/c568ab0c7e90c3fc122936fef0d4d489",
+            "image": "https://lh3.googleusercontent.com/p/AF1QipMbAwY9MBHgd_zAzO97Dd4R0fYrVicNIpaswOow=s1600-w1200",
             "title": "Das Schloss zu Biebrich",
             "author": "loehnertz",
             "liked": ["tester1", "tester2", "etawawd"]
