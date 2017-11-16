ea06d917bab73cef70399b6a9eee04d9d7ae0812
loehnertz <jakob2102@gmail.com>
2017-11-16 13:01:36 +0100

    Füge das Grundgerüst der 'backend.js' hinzu

---
+++ backend.js
@@ -0,0 +1,25 @@
+const Port = 3001;
+
+const Fs = require("fs");
+const Path = require("path");
+const Express = require("express");
+const Multer = require("multer");
+
+let app = Express();
+let upload = Multer({"dest": "./uploads/"});
+
+app.set('views', Path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(Express.urlencoded({ extended: true }));
+app.use('/uploads', Express.static('uploads'));
+app.use(Express.static('frontend'));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PATCH");
+    next();
+});
+
+app.listen(Port, function () {
+    console.log('Instaton-Server running...');
+});
